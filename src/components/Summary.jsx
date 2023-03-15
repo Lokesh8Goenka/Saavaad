fc1e9233b9d51b1d9b5cf03900b2da841085353c
Ritu Raj Pradhan <riturajpradhan911@gmail.com>
2023-03-15 15:08:11 +0530

    15-03-2023

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,49 +1,92 @@
-import React from 'react';
+import React, { useState} from 'react';
 
 const sumStyle = {
-    fontFamily: "'Shantell Sans', cursive",
-    color: "#fff",
-    marginLeft: "10vw",
-    paddingTop: "10vh",
-    fontSize: "6vw"
+  fontFamily: "'Shantell Sans', cursive",
+  color: "#fff",
+  marginLeft: "10vw",
+  paddingTop: "10vh",
+  fontSize: "6vw"
 }
 
-function handleSubmit(e) {
-    // Prevent the browser from reloading the page
-    e.preventDefault();
+function MLModelInput() {
+const [inputText, setInputText] = useState('');
+const [resultText, setResultText] = useState('');
 
-    // Read the form data
-    const form = e.target;
-    const formData = new FormData(form);
+const handleSubmit = async (event) => {
+  event.preventDefault();
 
-    // You can pass formData as a fetch body directly:
-    // fetch('/some-api', { method: form.method, body: formData });
+  if (inputText) {
+  // Send the input text to the ML model API endpoint
+    const response = await fetch('http://192.168.34.133:12345/translate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      text: inputText
+     })
+    });
 
-    // Or you can work with it as a plain object:
-    const formJson = Object.fromEntries(formData.entries());
-    console.log(formJson);
-}
+    // Parse the response as text
+    const resultText = await response.json();
 
-function Summary() {
-    return(
-        <div className="summary">
-                <h1 style={sumStyle}>Summary...</h1>
+    // Update the result state variable with the API response
+    setResultText(JSON.stringify(resultText));
+  }
 
-                <form method="post" onSubmit={handleSubmit}>
-                <button className="submitBtn" type="submit">Summarize</button>
-                <label style={{color: "#fff", float: "right", marginRight: "10vw"}}>
-                    <textarea
-                    name="postContent"
+  if(Image){
+    //Send the image to ml model model to detect the text and summarize
+    const formData = new FormData();
+    formData.append('image', file);
+
+    const response = await fetch('http://192.168.34.133:12345/image_text_summarize', {
+      method: 'POST',
+      body: formData
+    });
+
+    const imageText = await response.json();
+ 
+    // Update the result state variable with the API response
+    setResultText(JSON.stringify(imageText));
+  }
+};
+
+const handleChange = event => {
+  setInputText(event.target.value);
+};
+
+return (
+  <div className="summary">
+    <h1 style={sumStyle}>Summary...</h1>
+    <div className="row">
+      <div className="col" style={{padding: "0"}}>
+      <form onSubmit={handleSubmit}>
+                  <button className="submitBtn" type="submit">Submit</button>
+                  <label style={{color: "#fff", float: "right", marginRight: "10vw"}}> 
+                  <textarea style={{backgroundColor: "transparent", color: "white"}}
+                    type="text"
+                    id="text-input"
                     defaultValue="Your Text Here"
-                    rows={18}
-                    cols={70}
-                    />
-                </label>
-                <hr />
-                
-                </form>
-        </div>
-    );
+                    rows={10}
+                    cols={100}
+                    value={inputText}
+                    onChange={handleChange}
+                  />
+                  </label>
+      </form>
+      </div>
+    </div>
+    <div className="row">
+    <div className="col" style={{padding: "0"}}>
+      <output style={{backgroundColor: "transparent", color: "white", margin: "20px"}}>
+      {resultText}
+    </output>
+      </div>
+    </div>
+    
+   
+  </div>
+);
 }
 
-export default Summary;
+export default MLModelInput;
